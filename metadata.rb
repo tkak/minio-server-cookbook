8a6dea18c703c98d1c3da978892af582dc410a98
Takaaki Furukawa <takaaki.frkw@gmail.com>
2018-05-06 13:41:19 +0900

    Fix lint

--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,8 @@ long_description 'Installs/Configures Minio'
 version '0.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
+supports 'redhat', '>= 7.0'
+supports 'centos', '>= 7.0'
+
 issues_url 'https://github.com/tkak/minio-server-cookbook/issues'
 source_url 'https://github.com/tkak/minio-server-cookbook'
